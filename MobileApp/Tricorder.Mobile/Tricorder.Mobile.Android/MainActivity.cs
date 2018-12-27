507ecb34167fcca270b256b2c163853c20bfd555
Brett Sheleski <sheleski@fvtc.edu>
2018-12-27 14:40:34 -0600

    more stuff

--- MobileApp/Tricorder.Mobile/Tricorder.Mobile.Android/MainActivity.cs
+++ MobileApp/Tricorder.Mobile/Tricorder.Mobile.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Tricorder.Mobile.Droid
 {
-    [Activity(Label = "Tricorder.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Tricorder", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
